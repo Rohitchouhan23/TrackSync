78e4ed8e8c23c9b45fa96c88bdfdcaa5b37a93a3
rohitchouhan <rohitchouhan.1811@gmail.com>
2026-05-15 13:02:53 +0530

    updated 10 code

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(cors({
   credentials: true
 }));
 
-app.options("*", cors());
+app.options(/.*/, cors());
 
 /* ---------------- SOCKET ---------------- */
 const io = new Server(httpServer, {
